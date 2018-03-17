9d21e7ed47cbc1dd473b51b7f1e806bb7890d2ed
PrescottGallagher <p.rand.gallagher@gmail.com>
2018-03-17 05:04:09 -0400

    Bugfix

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 setInterval(function(){
 	for(var i = 0; i != 3; i++){
-		if(document.getElementsByClassName("irc_vpl irc_lth")[i].href != document.getElementsByClassName("irc_mimg irc_hic")[1].getElementsByClassName("irc_mi")[0].src)
+		if(document.getElementsByClassName("irc_vpl irc_lth")[i].href != document.getElementsByClassName("irc_mimg irc_hic")[i].getElementsByClassName("irc_mi")[0].src)
 		{
-			document.getElementsByClassName("irc_vpl irc_lth")[i].href = document.getElementsByClassName("irc_mimg irc_hic")[1].getElementsByClassName("irc_mi")[0].src;
+			document.getElementsByClassName("irc_vpl irc_lth")[i].href = document.getElementsByClassName("irc_mimg irc_hic")[i].getElementsByClassName("irc_mi")[0].src;
 			document.getElementsByClassName("irc_vpl irc_lth")[i].innerHTML = "<span class='Tl8XHc'>Open image in new tab</span>"
 		}
 	}
